76cce08b3b4088fe74c55271927d445ba9c147bc
Andrew Trinh <turinity57@gmail.com>
2023-02-07 06:37:02 -0500

    Finish hard coded user stats in get method, change package.json so you can use nodemon change eslint to 4 tab

--- server/routes/api.js
+++ server/routes/api.js
@@ -11,23 +11,43 @@ const router = express.Router();
 
 router.use(express.json());
 
-const apiRoute = "/api/";
 
-router.use("/", async function (_, res) {
-  console.log("here")
-  res.json("Success! Getting to the api!");
-});
+const userStat = "/user_stat"
 
-
-router.use(async function (_, res) {
-  res.status(404).json({ error: "Not Found" });
-});
 
 /*
 * Get method for the user stat
 */
-router.get(`${apiRoute}user_stat`, async () => {
-	
-})
+router.use(userStat, async (req, res) => {
+
+    // Get the name from the url and search for the stats in the database.
+    let user = req.query.name;
+    console.log(user);
+
+    // Should create a json object or just use the model and return it in res.json
+
+    // For now hard coded. 
+    let stats = {
+        "Avg Words/Min": 30,
+        "Accuracy": 74,
+        "Games Played": 80,
+        "Win": 34,
+        "Lose": 43,
+        "Draw": 23,
+        "Rank": 12 
+    };
+
+    res.status(200).json(stats);
+});
+
+router.use("/", async function (_, res) {
+    console.log("here")
+    res.json("Success! Getting to the api!");
+});
+
+
+router.use(async function (_, res) {
+    res.status(404).json({ error: "Not Found" });
+});
 
 export default router;
