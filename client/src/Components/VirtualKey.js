979ca2913cc713d5a864a1a345ac694785c15ddb
unknown <dallali.rim1@gmail.com>
2023-02-06 23:08:04 -0500

    Add single key component

---
+++ client/src/Components/VirtualKey.js
@@ -0,0 +1,9 @@
+import "./VirtualKey.css"
+
+function VirtualKey(props) {
+  return (
+    <div className={`keyboard-key ${props.classValue}`} id={props.keyCode}> {props.keyValue} </div>
+  );
+}
+
+export default VirtualKey;
