e25236a30cf6ed84cbd07193c5aa5dad18e72228
RimDallali <dallali.rim1@gmail.com>
2023-02-07 10:41:19 -0500

    fix shift press issue

--- client/src/Components/TextContainer.js
+++ client/src/Components/TextContainer.js
@@ -4,9 +4,9 @@ import { cleanUpLetter } from "../Controller/ConversionHelper"
 function TextContainer(props) {
   function handleOnChange(e) {
     let letter = e.nativeEvent.key
-    console.log(e.nativeEvent.key)
+
     if(letter === "Shift") {
-      onShiftPressed()
+      props.setKeyboard(props.allShiftKeys)
     }
     if (props.currentKeys.some(row => row.includes(letter))) {
       letter = cleanUpLetter(letter)
@@ -17,18 +17,13 @@ function TextContainer(props) {
       setTimeout(() => {
         currentKey.classList.remove("pressed")
       }, 100)
-    } else {
-      console.log("not here => " + letter)
     }
   }
 
-  function handleShiftRelease() {
-    props.setKeyboard(props.allRegKeys)
-  }
-
-  function onShiftPressed() {
-    console.log("shift is pressed")
-    props.setKeyboard(props.allShiftKeys)
+  function handleShiftRelease(e) {
+    if(e.nativeEvent.key === "Shift") {
+      props.setKeyboard(props.allRegKeys)
+    }
   }
 
   return (
@@ -36,7 +31,6 @@ function TextContainer(props) {
       className="text-container" 
       onKeyUp={e => handleShiftRelease(e)} 
       onKeyDown={e => handleOnChange(e)}>
-
     </textarea>
   )
 }
