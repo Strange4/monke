b9cc3848e8307a0f2f89c58c08f0a84ddacf313a
unknown <dallali.rim1@gmail.com>
2023-02-06 23:09:46 -0500

    link components together through main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import VirtualKeyboard from './Components/VirtualKeyboard';
+import TextContainer from './Components/TextContainer';
 import './App.css';
 
 function App() {
+  const allKeys =
+  [
+    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "="],
+    ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\"],
+    ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'"],
+    ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"],
+    [" "]
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextContainer allKeys={allKeys}/>
+      <VirtualKeyboard allKeys={allKeys}/>
     </div>
   );
 }
