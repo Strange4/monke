21389cd7672d63344e5d20de13cd568fdc1a6421
unknown <dallali.rim1@gmail.com>
2023-02-06 23:07:24 -0500

    add temporary text area to write test in that will detect change

---
+++ client/src/Components/TextContainer.js
@@ -0,0 +1,28 @@
+import "./TextContainer.css"
+import { cleanUpLetter } from "../Controller/ConversionHelper"
+
+function TextContainer(props) {
+  function handleOnChange(e) {
+    let letter = e.nativeEvent.data
+    if (props.allKeys.some(row => row.includes(letter))) {
+      letter = cleanUpLetter(letter)
+
+      let currentKey = document.querySelector(`#${letter}`)
+      currentKey.classList.add("pressed")
+      setTimeout(() => {
+        currentKey.classList.remove("pressed")
+      }, 100)
+    }
+    else {
+      console.log("not here " + props.allKeys)
+    }
+  }
+
+  return (
+    <textarea className="text-container" onChange={e => handleOnChange(e)}>
+
+    </textarea>
+  )
+}
+
+export default TextContainer
