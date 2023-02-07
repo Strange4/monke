9b7f71a8b0f8e8b86a9d2fffe0761669494144ca
unknown <dallali.rim1@gmail.com>
2023-02-06 20:43:08 -0500

    print full clickable link to started server

--- server/bin/www.js
+++ server/bin/www.js
@@ -9,5 +9,5 @@ import app from "../routes/app.js";
 const PORT = process.env.PORT || 8080; 
 
 app.listen(PORT, function(){
-  console.log("Server Started on port: " + PORT);
+  console.log("Server Started on port: http://localhost:" + PORT);
 });
