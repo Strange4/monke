d136f2743b15b1c49ebd3dbd8ed58625a075b250
Andrew T <turinity57@gmail.com>
2023-02-08 09:16:11 -0500

    Add database and models folder, started mongo.js

---
+++ server/database/mongo.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const dbUrl = process.env.ATLAS_URI;
+let conn = null;
+/**
+ * connects to the database and returns the connection
+ * Throws error if the connection to the db can't be made
+ */
+const getDBConnection = async () => {
+    if(conn === null){
+        mongoose.set('strictQuery', true);
+        conn = await mongoose.connect(dbUrl, {dbName: ""});
+    }
+    return conn;
+}
+
+export default getDBConnection;
