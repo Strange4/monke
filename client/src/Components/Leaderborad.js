7f2b2544349f5456f1e5ee9bb9db4f1ef0216ad6
Katharina <katharina.orfanidis@dawsoncollege.qc.ca>
2023-02-11 18:52:58 -0500

    Fix layout styling for leaderboard display

--- client/src/Components/Leaderborad.js
+++ client/src/Components/Leaderborad.js
@@ -8,10 +8,30 @@ const Leaderboard = () => {
         <div id="leaderboard">
             <h1>Leaderboard</h1>
             <div id="rankings">
+                <div id="leaderboard-header">
+                    <p>Rank</p>
+                    <p>User</p>
+                    <p>WPM</p>
+                    <p>Date</p>
+                </div>
                 <RankListItem
                     rank="1"
-                    username="Name"
+                    username="Name1"
                     wpm="200"
+                    date={`${date}`}>
+
+                </RankListItem>
+                <RankListItem
+                    rank="2"
+                    username="Name2"
+                    wpm="192"
+                    date={`${date}`}>
+
+                </RankListItem>
+                <RankListItem
+                    rank="3"
+                    username="Name3"
+                    wpm="185"
                     date={`${date}`}>
 
                 </RankListItem>
--- client/src/Components/RankListItem.jsx
+++ client/src/Components/RankListItem.jsx
@@ -2,10 +2,9 @@ import './Styles/RankListItem.css';
 
 const RankListItem = (props) => {
     return (
-        <div id="rank-list-item">
+        <div className="rank-list-item">
             <p>{props.rank}</p>
-            <img src={`${props.user_image}`}></img>
-            <p>{props.username}</p>
+            <p><img src={`${props.user_image}`}></img>{props.username}</p>
             <p>{props.wpm}</p>
             <p>{props.date}</p>
         </div>
