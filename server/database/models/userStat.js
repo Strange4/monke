b2b3825e8f484074d839a26687ab897088498f80
Andrew T <turinity57@gmail.com>
2023-02-13 12:11:10 -0500

    Change schema for quote and userstat

--- server/database/models/userStat.js
+++ server/database/models/userStat.js
@@ -7,13 +7,15 @@ const Schema = mongoose.Schema;
  */
 const UserStatSchema = new Schema({
     user: {type : Schema.Types.ObjectId, ref: "User"},
+    max_wpm: Number,
     wpm: Number,
+    max_accuracy: Number,
     accuracy: Number,
-    games: Number,
+    games_count: Number,
     win: Number,
     lose: Number,
     draw: Number,
-    rank: Number
+    max_date: Date
 });
 
 const UserStat = mongoose.model("UserStat", UserStatSchema);
