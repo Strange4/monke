67f4f14db145b4c38de6303db85b3eb98f1cdd2d
Andrew Trinh <turinity57@gmail.com>
2023-02-13 23:10:26 -0500

    fix mongo connection error

--- server/database/mongo.js
+++ server/database/mongo.js
@@ -7,15 +7,13 @@ const dbUrl = process.env.ATLAS_URI;
  * connects to the database and returns the connection
  * Throws error if the connection to the db can't be made
  */
-let connectToDatabase = async () => {
-    let conn;
+const connectToDatabase = async () => {
     try{
         mongoose.set('strictQuery', true);
-        conn = await mongoose.connect(dbUrl, {dbName: "QuotesDatabase"});
+        await mongoose.connect(dbUrl, {dbName: "QuotesDatabase"});
     } catch (err) {
         throw new Error("Could not connect");
     }
-    return conn;
 }
 
 export default connectToDatabase;
