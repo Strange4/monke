fe3987685cde9f33e1b79472fef6cbf4c263c61e
Katharina <katharina.orfanidis@dawsoncollege.qc.ca>
2023-02-14 20:17:37 -0500

    Start user page layout

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,11 +1,18 @@
 import NavBar from "../Components/NavBar";
+import "./Styles/Profile.css"
 
-const Profile = () => {
+const Profile = (props) => {
     return (
         <div id="home">
             <NavBar />
-            <h1>Profile Page</h1>
-            <h2>Will show different content based on login status</h2>
+            <div id="profile">
+                <img id="profile-pic" src={`${props.profilePicture}`}></img>
+                <div id="user-info">
+                    <h2>Name: {props.username}</h2>
+                    <h2>Rank: {props.rank}</h2>
+                </div>
+
+            </div>
         </div>
     );
 }
