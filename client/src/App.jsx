7a9da79954a2f26e78a9b99110241a6779e49b5c
RimDallali <dallali.rim1@gmail.com>
2023-02-21 16:51:23 -0500

    set up OAUTH and add client side component got google login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/App.css'
 import Home from './Pages/Home';
 import Profile from './Pages/Profile';
+import Login from './Components/Login';
 
 /**
  * Displays the Main App container
  * @returns {ReactElement}
  */
 function App() {
+
     return (
         <div className="App">
             <Router>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/profile" 
-                        element={<Profile profilePicture={""} 
-                        />} />
+                    <Route path="/profile" element={<Login profilePicture={""}/>} />
                 </Routes>
             </Router>
             <div id="popup-root" />
--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,12 +1,35 @@
 import './Styles/Login.css';
+import { GoogleLogin } from '@react-oauth/google';
 
-const Login = () => {
+function Login() {
+
+    const handleLogin = async googleData => {
+        const res = await fetch("/auth", {
+            method: "POST",
+            body: JSON.stringify({
+                token: googleData.credential
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const data = await res.json()
+        // we will come back to this, since our server will be replying with our info
+    }
+
+    const handleError = error => {
+        console.error(error);
+    }
+
     return (
         <div id="login">
             <h1>Login Popup</h1>
-            <button id="login-btn">Google Login Button</button>
+            <GoogleLogin
+                onSuccess={handleLogin}
+                onError={handleError}
+            />
         </div>
     );
-};
+}
 
 export default Login;
