17440a500f6b818a7559d8eb9d9eba5b217876d6
RimDallali <dallali.rim1@gmail.com>
2023-02-21 19:23:48 -0500

    add new router for handling google authentication

---
+++ server/routes/authentication.js
@@ -0,0 +1,96 @@
+import * as express from "express";
+
+import { OAuth2Client } from 'google-auth-library';
+import session from 'express-session'
+import dotenv from 'dotenv';
+dotenv.config();
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+const router = express.Router();
+router.use(express.json());
+
+//temp until we connect the database to users
+const users = new Array();
+
+
+//middleware to verify the session
+function isAuthenticated(req, res, next) {
+    if (!req.session.user) {
+        return res.sendStatus(401);
+    }
+    next();
+}
+
+router.use(session({
+    secret: process.env.SECRET,
+    name: 'id',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 120000,
+        //should only sent over https, but set to false for testing and dev on localhost
+        secure: false,
+        httpOnly: true,
+        sameSite: 'strict'
+    }
+}));
+
+router.post("/auth", async (req, res) => {
+    //TODO: should validate that the token was sent first
+    const { token } = req.body;
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.GOOGLE_CLIENT_ID
+    });
+    if (!ticket) {
+        //unauthorized (token invalid)
+        return res.sendStatus(401);
+    }
+    const user = { 
+        "name": ticket.getPayload().name, 
+        "email": ticket.getPayload().email, 
+        "picture": ticket.getPayload().picture 
+    };
+    const existsAlready = users.findIndex(element => element.email === ticket.getPayload().email);
+    if (existsAlready < 0) {
+        //insert
+        users.push(user);
+    } else {
+        //update
+        users[existsAlready] = user;
+    }
+    //create a session, using email as the unique identifier
+    req.session.regenerate(function (err) {
+        if (err) {
+            return res.sendStatus(500);
+        }
+        //store the user's info in the session
+        req.session.user = user;
+        res.json({ user: user });
+    });
+});
+
+//route for authenticated users only
+router.get("/protected",
+    isAuthenticated,
+    function (_, res) {
+        //would actually be doing something
+        res.sendStatus(200);
+    }
+);
+
+router.get("/logout", isAuthenticated, function (req, res) {
+    //destroy the session
+    req.session.destroy(function (err) {
+        //callback invoked after destroy returns
+        if (err) {
+            //server error, couldn't destroy the session
+            return res.sendStatus(500); 
+        }
+        //clear the cookie
+        res.clearCookie('id'); 
+        res.sendStatus(200);
+    });
+});
+
+export default router;
