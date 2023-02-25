ab3dfcad3cd5e00186d1727c270244155a7b5306
Christopher Hu <christopherhu1212@gmail.com>
2023-02-24 20:46:17 -0500

    create database class+add func to see if connected

--- server/database/mongo.js
+++ server/database/mongo.js
@@ -3,17 +3,43 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const dbUrl = process.env.ATLAS_URI;
+const DISCONNECTED = 0;
+const CONNECTED = 1;
+const DB_NAME = "QuotesDatabase";
+
 /**
- * connects to the database and returns the connection
- * Throws error if the connection to the db can't be made
+ * Database class that uses mongoose to connect to a mongodb atlas database.
+ * Contains functions to connect to a database and check connection status.
  */
-const connectToDatabase = async () => {
-    try{
-        mongoose.set('strictQuery', true);
-        await mongoose.connect(dbUrl, {dbName: "QuotesDatabase"});
-    } catch (err) {
-        throw new Error("Could not connect" + err);
+class Database {
+    constructor(){
+        // immediately establish a connection with the database if disconnected
+        if(this.isConnected() === DISCONNECTED){
+            this.connectToDatabase(); 
+        }
+    }
+
+    /**
+     * async method to establish a connection with the database.
+     */
+    async connectToDatabase () {
+        try{
+            mongoose.set('strictQuery', true);
+            console.log(`Attempting to connect to database: ${DB_NAME}`);
+            await mongoose.connect(dbUrl, { dbName: DB_NAME });
+            console.log(`Connected to database: ${DB_NAME}`);
+        } catch (err) {
+            console.error("could not connect to server", err);
+        }
+    }
+
+    /**
+     * returns true if online connection to database is established:
+     * @returns boolean
+     */
+    isConnected() {
+        return mongoose.connection.readyState === CONNECTED ? true : false
     }
 }
 
-export default connectToDatabase;
+export default Database;
