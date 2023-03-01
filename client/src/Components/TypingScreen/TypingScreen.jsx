0f34a095c759c61d3379e711922a632c3f12faa5
Strange4 <>
2023-03-01 10:12:18 -0500

    add: break down typing screen utils

--- client/src/Components/TypingScreen/TypingScreen.jsx
+++ client/src/Components/TypingScreen/TypingScreen.jsx
@@ -3,11 +3,13 @@ import { useState, useRef } from "react";
 import VirtualKeyboard from './VirtualKeyboard';
 import keyboardKeys from "../../Data/keyboard_keys.json";
 import GameText from '../GameText';
-import Chronometer from './Chronometer';
-import { useChronometer } from './Chronometer';
+import Chronometer, {useChronometer} from './Chronometer';
 import SoloGameResult from './SoloGameResult';
 import { useQuery } from 'react-query';
 import Spinner from '../Spinner';
+import { 
+    renderLetters, getDefaultUserDisplay, mapKeyToKeyboard, preventDefaultBehavior 
+} from './TypingUtil';
 
 const allShiftKeys = keyboardKeys.english.upper;
 const allRegKeys = keyboardKeys.english.lower;
@@ -34,14 +36,14 @@ function TypingScreen() {
     function handleGameEnd(){
         stopTimer();
         setDisplayResults(true);
+        setUserDisplay([]);
+        refetch();
     }
     
     function resetGame(){
         setDisplayResults(false);
         resetTimer();
         textContainerRef.current.value = "";
-        setUserDisplay([]);
-        refetch();
     }
 
     /**
@@ -132,98 +134,6 @@ function TypingScreen() {
                 </>}
         </div>
     );
-}
-
-/**
- * prevents the deafult behavior from an event
- * @param {Event} e the event to prevent
- */
-function preventDefaultBehavior(e){
-    e.preventDefault();
-}
-
-
-/**
- * changes the type (right | wrong | none) of the display of letters based on the user input
- * @param {string} newInput the new input that has been changed by the user
- * @param {{ letter: string; type: string; current: boolean; }[]} display the current display state
- */
-function renderLetters(newInput, display) {
-    const newLetterIndex = newInput.length - 1;
-    const newDisplay = display;
-
-    // are are setting all the next letters that could posibly be deleted to none.
-    for (let i = newLetterIndex + 1; i < newDisplay.length; i++) {
-        newDisplay[i].type = "none";
-        newDisplay[i].current = false;
-    }
-    
-    const inputIsEmpty = newInput.length === 0;
-    const inputIsDone = newInput.length > newDisplay.length;
-    // if the input is done don't do anything
-    if(inputIsDone){
-        return display;
-    }
-    const nextLetter = newDisplay[newLetterIndex + 1];
-    if(nextLetter){
-        nextLetter.current = true;
-    }
-
-    if(!inputIsEmpty){
-        const newLetter = newDisplay[newLetterIndex];
-        newLetter.current = false;
-        if(newLetter.letter === newInput[newLetterIndex]){
-            newLetter.type = "right";
-        } else {
-            newLetter.type = "wrong";
-        }
-    }
-    return newDisplay;
-}
-
-/**
- * Get's the default user display filler with letter object with type none.
- * @param {string} stringToDisplay the original string that needs to be transformed
- */
-function getDefaultUserDisplay(stringToDisplay) {
-    if(!stringToDisplay){
-        return [];
-    }
-    const display = Array.from(stringToDisplay).map((letter) => {
-        // the type of a displayed letter can only be right | wrong | none
-        return {
-            letter,
-            type: "none",
-            current: false
-        }
-    });
-    if(display[0]){
-        display[0].current = true;
-    }
-    return display;
-}
-
-/**
- * maps keyboard keys to an array of pressed and unpressed keys
- * if a prevous state if defined, maps the new keys to the previous state (pressed or unpressed)
- * @param {string[][]} keys the keys that need to me mapped
- * @param {{ keyValue: string; isPressed: boolean; }[][]?} previousState
- */
-function mapKeyToKeyboard(keys, previousState){
-    return keys.map((row, rowIndex) => {
-        return row.map((keyValue, keyValueIndex) => {
-            if(previousState){
-                return {
-                    keyValue,
-                    isPressed: previousState[rowIndex][keyValueIndex].isPressed
-                }
-            }
-            return {
-                keyValue,
-                isPressed: false
-            }
-        })
-    })
 }
 
 export default TypingScreen;
