b12e6bb1304a9a0391d5f7a6a90682b27ba4f4e0
Katharina <katharina.orfanidis@dawsoncollege.qc.ca>
2023-03-05 14:35:09 -0500

    Add link to multiplayer game page to play button in lobby

--- client/src/Pages/Lobby.jsx
+++ client/src/Pages/Lobby.jsx
@@ -5,6 +5,7 @@ import GameSettings from "../Components/GameSettings";
 import { AiFillSetting } from "react-icons/ai"
 import PlayerItem from '../Components/PlayerItem';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
@@ -28,7 +29,9 @@ const Home = () => {
 
                     <button id="invite-btn">INVITE</button>
                 </div>
-                <button id="play-btn">PLAY</button>
+                <Link to="/multiplayer-game">
+                    <button id="play-btn">PLAY</button>
+                </Link>
                 {settings && <GameSettings id="lobby-settings"/> }
                 <AiFillSetting id="lobby-settings-icon" onClick={handleClick}/>
             </div>
