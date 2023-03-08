85fe08808cd8a2cf9d548677c348d8e5e8ed32ca
Katharina <katharina.orfanidis@dawsoncollege.qc.ca>
2023-03-08 13:57:36 -0500

    Fix naming in lobby page, Replace INVITE button with lobby code and copy icon, Add comments about futur functionality

--- client/src/Pages/Lobby.jsx
+++ client/src/Pages/Lobby.jsx
@@ -6,10 +6,13 @@ import { AiFillSetting } from "react-icons/ai"
 import PlayerItem from '../Components/PlayerItem';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { BiCopy } from 'react-icons/bi'
 
-const Home = () => {
+const Lobby = () => {
 
     const [settings, showSettings] = useState(false);
+    // add state to handle wheter a user is the lobby creator
+    // Will show additional info/settings if they are
 
     const handleClick = () => {
         
@@ -21,14 +24,17 @@ const Home = () => {
             <NavBar />
             <div id="lobby-info">
                 <div id="players">
-                    {/* tobe replaced with a dynamic list as players join */}
+                    {/* to be replaced with a dynamic list as players join */}
                     <PlayerItem name="Name"/>
                     <PlayerItem name="Name"/>
                     <PlayerItem name="Name"/>
                     <PlayerItem name="Name"/>
                     <PlayerItem name="Name"/>
 
-                    <button id="invite-btn">INVITE</button>
+                    {/* text to be replaced with the generated lobby code */}
+                    {/* icon will have a copy function to copy the lobby code */}
+                    <p id="invite-code">TEMP TEXT <BiCopy id="copy-icon"/></p>
+
                 </div>
                 <Link to="/multiplayer-game">
                     <button id="play-btn">PLAY</button>
@@ -44,4 +50,4 @@ const Home = () => {
     );
 }
 
-export default Home;
+export default Lobby;
