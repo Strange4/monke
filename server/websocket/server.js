fec76e4b088792712e7bf9495f295a18fd690415
unknown <dallali.rim1@gmail.com>
2023-03-12 00:35:14 -0500

    implement joining room listeners

--- server/websocket/server.js
+++ server/websocket/server.js
@@ -2,18 +2,25 @@ import { Server } from "socket.io"
 
 export function setUp(server) {
     const io = new Server(server, { cors: { origin: "http://localhost:3000" } })
+    let userList = []
 
     io.on("connection", (socket) => {
         console.log("CONNECTED")
         
-        if(socket.handshake.auth.userEmail) {
+        let roomCode = socket.handshake.query.roomCode
+        let userData = { username: "" }
+
+        if (socket.handshake.auth.userEmail) {
             console.log(`LOGGED IN as ${socket.handshake.auth.userEmail}`)
+            userData["username"] = socket.handshake.auth.userEmail
         } else {
+            //TODO set random name and picture here
             console.log("not logged in")
+            userData["username"] = "GUEST"
         }
 
-        socket.on("join-room", (roomID, userEmail) => {
-            console.log(roomID)
-        })
+        socket.join(roomCode);
+        userList.push({username: userData.username})
+        io.to(roomCode).emit("join-room", userList)
     });
 }
