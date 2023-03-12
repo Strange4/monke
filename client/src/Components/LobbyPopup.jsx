ffdffb5a4557df1c1994a7c203fc407c043339b2
unknown <dallali.rim1@gmail.com>
2023-03-12 00:35:05 -0500

    implement joining room listeners

--- client/src/Components/LobbyPopup.jsx
+++ client/src/Components/LobbyPopup.jsx
@@ -1,7 +1,7 @@
 import './Styles/LobbyPopup.css';
 import './Styles/Popup.css'
 import { Link } from 'react-router-dom';
-import { useState, useContext } from 'react';
+import { useState, useContext, useRef } from 'react';
 import AuthContext from '../Context/AuthContext';
 import * as FetchModule from "../Controller/FetchModule"
 import { useNavigate } from "react-router-dom";
@@ -13,15 +13,30 @@ import { io } from "socket.io-client";
  */
 function LobbyPopup() {
     const [code, enterCode] = useState(false);
+    const roomCode = useRef()
     const navigate = useNavigate()
     const auth = useContext(AuthContext);
     let socket
-    let users = []
+
+    const handleClick = () => {
+        enterCode(current => !current)
+    }
 
     const joinLobby = () => {
-        enterCode(current => !current)
+        navigate("/lobby", { state: { roomCode: roomCode.current.value } });
+        console.log(roomCode.current.value)
+        socket = io("", { query: { roomCode: roomCode.current.value }, auth: { userEmail: auth.userEmail } })
 
-        
+        socket.on("join-room", (users) => {
+            // users.push({ username: userData.username })
+            console.log("JOINED ROOM")
+            console.log(users)
+            // socket.emit("join-room", userData, roomID)
+        })
+
+        // socket.on("leave-room", (userData, roomID, users) => {
+        //     users.filter(user => user != userData.username)
+        // })
     }
 
     function createLobby() {
@@ -30,14 +45,12 @@ function LobbyPopup() {
             navigate("/lobby", { state: { roomCode: newRoomCode } });
             socket = io("", { query: { roomCode: newRoomCode }, auth: { userEmail: auth.userEmail } })
 
-            socket.on("join-room", (userData, roomID) => {
-                users.push({ username: userData.username })
-                console.log("JOINED ROOM")
-                console.log(userData)
-                console.log(roomID)
-                console.log(socket)
-                console.log(users)
+            socket.on("join-room", (users) => {
+                // userList.push({ username: userData.username })
+                console.log("CREATED AND JOINED ROOM")
                 // socket.emit("join-room", userData, roomID)
+                // console.log(userList)
+                console.log(users)
             })
         })()
     }
@@ -47,13 +60,11 @@ function LobbyPopup() {
         <div id="lobby" className='popup'>
             <h1>Lobby Popup</h1>
             <button onClick={createLobby}>Create</button>
-            <button onClick={joinLobby}>Join</button>
+            <button onClick={handleClick}>Join</button>
             {code &&
                 <div>
-                    <input type="text" name="code" />
-                    <Link to='/lobby'>
-                        <button>Enter game</button>
-                    </Link>
+                    <input ref={roomCode} type="text" name="code" />
+                    <button onClick={joinLobby}>Enter game</button>
                 </div>
             }
         </div>
