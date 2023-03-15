3317243b1b5c67a4b769e52cd39e4b9d22abc03f
unknown <dallali.rim1@gmail.com>
2023-03-15 19:53:48 -0400

    split up web socket server into multiple functions

--- server/websocket/server.js
+++ server/websocket/server.js
@@ -1,39 +1,44 @@
 import { Server } from "socket.io"
 
-const defaultAvatar = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-
 export function setUp(server) {
     const io = new Server(server, { cors: { origin: "http://localhost:3000" } })
     let userDict = {}
 
     io.on("connection", (socket) => {
-        let roomCode = socket.handshake.query.roomCode
-        let userData = { username: "", id: "", avatar: "" }
+        const userData = setUserData(socket)
 
-        userData["username"] = socket.handshake.auth.userData?.username || "GUEST"
-        userData["avatar"] = socket.handshake.auth.userData?.avatar || defaultAvatar
-        userData["id"] = socket.id
+        const roomCode = socket.handshake.query.roomCode
+        userDict[roomCode] = userDict[roomCode] || []
 
-        socket.join(roomCode);
-
-        if (!userDict[roomCode]) {
-            userDict[roomCode] = []
-        }
-
-        socket.on("try-join", () => {
-            if (userDict[roomCode].length < 2) {
-                userDict[roomCode].push(userData)
-                io.to(roomCode).emit("join-room", userDict[roomCode], roomCode)
-            } else {
-                socket.emit("full-room")
-            }
-        })
-
-        socket.on("disconnect", () => {
-            userDict[roomCode] = userDict[roomCode].filter(user => user.id !== userData.id)
-            io.to(roomCode).emit("leave-room", userDict[roomCode], roomCode)
-            socket._cleanup()
-        })
+        setUpLobbyListeners(socket, userData, roomCode)
     });
+}
 
+function setUpLobbyListeners(socket, userData, roomCode) {
+    socket.on("try-join", () => {
+        if (userDict[roomCode].length < 2) {
+            socket.join(roomCode);
+            userDict[roomCode].push(userData)
+            io.to(roomCode).emit("join-room", userDict[roomCode], roomCode)
+        } else {
+            socket.emit("full-room")
+        }
+    })
+
+    socket.on("disconnect", () => {
+        userDict[roomCode] = userDict[roomCode].filter(user => user.id !== userData.id)
+        io.to(roomCode).emit("leave-room", userDict[roomCode], roomCode)
+        socket._cleanup()
+    })
+}
+
+function setUserData(socket) {
+    const defaultAvatar = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+    let userData = { username: "", id: "", avatar: "" }
+
+    userData["username"] = socket.handshake.auth.userData?.username || "GUEST"
+    userData["avatar"] = socket.handshake.auth.userData?.avatar || defaultAvatar
+    userData["id"] = socket.id
+
+    return userData
 }
