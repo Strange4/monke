3f3a7ccaf65dd4b373054dad5079d085f0cfeb04
Andrew Trinh <turinity57@gmail.com>
2023-03-16 16:45:29 -0400

    Change quote schema to match the new schema from the database

--- server/database/models/quote.js
+++ server/database/models/quote.js
@@ -7,7 +7,11 @@ const Schema = mongoose.Schema;
  */
 const QuoteSchema = new Schema({
     "quote": { type: String, required: true },
-    "difficulty": {type: Number, required: true}
+    "difficulty": {type: Number, required: true},
+    "number_characters": Number,
+    "number_special_characters": Number,
+    "number_words": Number,
+    "average_word_length": Number
 });
 
 const Quote = mongoose.model("Quote", QuoteSchema);
