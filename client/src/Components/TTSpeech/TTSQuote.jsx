10d6f4a263bcc992c8c088d878fe5f80d65b4487
Christopher Hu <christopherhu1212@gmail.com>
2023-03-23 18:15:30 -0400

    add documentation for TTSQuote

--- client/src/Components/TTSpeech/TTSQuote.jsx
+++ client/src/Components/TTSpeech/TTSQuote.jsx
@@ -1,6 +1,14 @@
 import Speech from "react-speech";
 import { useRef, useEffect, useState } from "react";
 
+/**
+ * React component that converts a quote spoken words using the 'react-speech' library.
+ * @param {string} text - Text to be read by react-speech.
+ * @param {boolean} resultScreenOff - Flag indicating if result screen is active.
+ * @param {boolean} enabled - Flag indicating if the user enbled the TTS feature.
+ * @returns {ReactElement|null} - If result screen inactive and TTS enabled, returns TTS component.
+ * Returns null if TTS is not enabled or result screen is active.
+ */
 function TTSQuote({ text, resultScreenOff, enabled }){
 
     const [isMounted, setIsMounted] = useState(false);
