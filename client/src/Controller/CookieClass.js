dda77dfe642a38fbfd4bd5dcfe16162c44dcd774
Andrew T <turinity57@gmail.com>
2023-03-23 10:46:38 -0400

    Finish cookie functions in new controller file

---
+++ client/src/Controller/CookieClass.js
@@ -0,0 +1,29 @@
+import Cookies from "js-cookie";
+
+/**
+ * Sets the cookie in the browser with the given name, value and expiry date of current date + 100 000 days.
+ * @param {string} cookieName, the name of the cookie that will be created. 
+ * @param {string} cookieValue, the value of the cookie that will be created.
+ */
+function setCookie(cookieName, cookieValue){
+    Cookies.set(cookieName, cookieValue, {expires: 100000});
+}
+
+/**
+ * Checks if the cookie exist and gets the value if it does.
+ * @param {string} cookieName,  the name of the cookie. 
+ * @returns the value of the cookie associated with the name of the cookie name.
+ */
+function getCookieValue(cookieName){
+    return Cookies.get(cookieName);
+}
+
+/**
+ * Deletes the cookie with the given name.
+ * @param {string} cookieName, the name of the cookie.
+ */
+function deleteCookie(cookieName){
+    Cookies.remove(cookieName);
+}
+
+export default {setCookie, getCookieValue, deleteCookie};
