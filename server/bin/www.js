8fe423bfce543b069a83c02e5a9467661c5b131d
Strange4 <>
2023-03-24 09:02:30 -0400

    change express static location to app and change redirect

--- server/bin/www.js
+++ server/bin/www.js
@@ -2,12 +2,7 @@
  * this module imports the app module and starts a local express server
  */
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 import app from "../routes/app.js";
-import express from "express"
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import { setUp } from '../websocket/server.js';
@@ -21,8 +16,6 @@ const PORT = process.env.EXPRESS_PORT || 8080;
     } catch(_){
         console.log("Couldn't connect to the database");
     }
-    const buildPath = path.resolve(__dirname, "..", "..", "client", "build");
-    app.use(express.static(buildPath));
     let server = app.listen(PORT, () => {
         console.log("Server Started on port: http://localhost:" + PORT);
     })
