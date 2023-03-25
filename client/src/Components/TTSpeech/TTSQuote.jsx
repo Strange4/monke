1f2cfe49bc90a876588c1c717e6446837f9ac3e0
Christopher Hu <christopherhu1212@gmail.com>
2023-03-25 19:53:20 -0400

    change TTSQuote so that it is always visible

--- client/src/Components/TTSpeech/TTSQuote.jsx
+++ client/src/Components/TTSpeech/TTSQuote.jsx
@@ -6,8 +6,7 @@ import { useRef, useEffect, useState } from "react";
  * @param {string} text - Text to be read by react-speech.
  * @param {boolean} resultScreenOff - Flag indicating if result screen is active.
  * @param {boolean} enabled - Flag indicating if the user enbled the TTS feature.
- * @returns {ReactElement|null} - If result screen inactive and TTS enabled, returns TTS component.
- * Returns null if TTS is not enabled or result screen is active.
+ * @returns {ReactElement} - If result screen inactive and TTS enabled, returns TTS component.
  */
 function TTSQuote({ text, resultScreenOff, enabled }){
 
@@ -15,19 +14,28 @@ function TTSQuote({ text, resultScreenOff, enabled }){
     const speechRef = useRef();
 
     useEffect(() => {
-        if(resultScreenOff && enabled){
-            setIsMounted(true);
-        }
+        resultScreenOff && enabled ? setIsMounted(true) : setIsMounted(false);
     }, [resultScreenOff, enabled]);
 
     useEffect(() => {
         if(speechRef.current && isMounted){
-            speechRef.current.play();
-            setIsMounted(false);
+            playQuote();
         }
     }, [isMounted]);
 
-    return isMounted ? <Speech ref={speechRef} text={text}/> : null;
+    function playQuote(){
+        speechRef.current.play();
+    }
+
+    return isMounted && <Speech ref={speechRef} text={text}
+        displayText="Replay quote"
+        textAsButton={true}
+        onKeyUp={(e) => {
+            if(e.key === 'Enter'){
+                playQuote();
+            }
+        }}
+    />;
 }
 
 export default TTSQuote;
