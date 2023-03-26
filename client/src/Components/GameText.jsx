305f3ce68b6ce52efb42ec6882134f199e86cda6
Strange4 <>
2023-03-25 20:51:08 -0400

    add transition to text cursor and remove mouse when typing

--- client/src/Components/GameText.jsx
+++ client/src/Components/GameText.jsx
@@ -1,13 +1,33 @@
+import { useEffect, useRef } from 'react';
 import './Styles/GameText.css';
-
+/**
+ * 
+ * @param {{display: {letter: string; type: string; current: boolean; }[]}} param0 
+ * @returns 
+ */
 function GameText({ display, isFocused, onClick }) {
+    const caretRef = useRef();
+    const currentLetterRef = useRef();
 
-    const caret = <span id="inputCaret"></span>;
-
+    // shout out to noah for coming in clutch with a big brain idea
+    requestAnimationFrame(() => {
+        if(!caretRef.current || !currentLetterRef.current){
+            return;
+        }
+        const currentLetter = currentLetterRef.current;
+        const rect = caretRef.current.getBoundingClientRect();
+        const height = rect.height;
+        const width = rect.width;
+        const top = currentLetter.offsetTop - height / 8;
+        const left = currentLetter.offsetLeft - width;
+        caretRef.current.style.top = `${top}px`;
+        caretRef.current.style.left = `${left}px`;
+    });
     return (
-        <div id="typing-container">
+        <div id="typing-container"
+            style={{ cursor: isFocused ? 'none' : 'auto' }}>
             <div
-                style={{display: isFocused ? 'none' : 'inline-block' }}
+                style={{ display: isFocused ? 'none' : 'inline-block' }}
                 onClick={onClick} 
                 id="text-focus-message">
                 👆 Click here to start typing
@@ -16,13 +36,14 @@ function GameText({ display, isFocused, onClick }) {
                 id="typing-screen-text"
                 onClick={onClick} 
                 className={isFocused ? 'letter-container' : 'blurred-container letter-container'}>
+                <span ref={caretRef} id="inputCaret"></span> 
                 {
                     display.map((letter, index) => {
                         return (
                             <span
+                                ref={letter.current ? currentLetterRef : undefined}
                                 key={index}
                                 className={`${letter.type} letter ${letter.current}`} >
-                                {letter.current ? caret : undefined}
                                 {letter.letter}
                             </span>
                         )
