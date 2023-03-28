6ef63b2a08b39526aba1ec442be4a335088bb30b
Katharina Orfanidis <katharina.orfanidis@dawsoncollege.qc.ca>
2023-03-27 23:33:44 -0400

    Fix end game leaderboard styling

--- client/src/Components/MultiplayerGame/EndGameResults.jsx
+++ client/src/Components/MultiplayerGame/EndGameResults.jsx
@@ -3,18 +3,26 @@ const EndGameResults = (props) => {
         <div id="game-results" className="player-info">
             <img className="player-image" src={`${props.avatar}`} />
             <p className='player-name'> {props.name} </p>
-            <div id="game-single-result">
-                {props.ended ?
+            {props.ended ?
+                <div id="game-single-result">
                     <p>
                         RANK: {props.rank + 1}
+                    </p>
+                    <p>
                         wpm: {props.wpm}
-                        accuracy: {props.acc}
+                    </p>
+                    <p>
+                        acc: {props.acc}
+                    </p>
+                    <p>
                         score: {Math.round(props.wpm * props.acc)}
                     </p>
-                    :
+                </div>
+                :
+                <div id="game-single-result">
                     <p> User did not finish </p>
-                }
-            </div>
+                </div>
+            }
         </div>
     );
 }
