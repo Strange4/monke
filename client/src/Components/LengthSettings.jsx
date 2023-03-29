5b12cf13e418ea94a187739785fa6c051e3976d1
Andrew T <turinity57@gmail.com>
2023-03-29 03:40:37 -0400

    add functionality to button length and added a random button

--- client/src/Components/LengthSettings.jsx
+++ client/src/Components/LengthSettings.jsx
@@ -5,11 +5,29 @@ import './Styles/Settings.css';
  * @returns {ReactElement}
  */
 function LengthSettings() {
+
+    async function getQuote(e){
+        const url = `/api/quote?quoteLength=${e.target.textContent}`;
+        const data = await fetch(url);
+        const json = await data.json();
+        const quote = json.body;
+        console.log(quote);
+    }
+
+    async function getRandom(){
+        const url = `/api/quote`;
+        const data = await fetch(url);
+        const json = await data.json();
+        const quote = json.body;
+        console.log(quote);
+    }
+
     return (
         <div id="length-settings">
-            <button>short</button>
-            <button>medium</button>
-            <button>long</button>
+            <button type="button" onClick={getQuote}>short</button>
+            <button type="button" onClick={getQuote}>medium</button>
+            <button type="button" onClick={getQuote}>long</button>
+            <button type="button" onClick={getRandom}>random</button>
         </div>
     );
 }
