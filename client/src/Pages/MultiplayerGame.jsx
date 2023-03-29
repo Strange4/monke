82f362f6a091216f7dbd4d29d658a200a0a256a7
Katharina Orfanidis <katharina.orfanidis@dawsoncollege.qc.ca>
2023-03-29 09:57:01 -0400

    Show name of players playing in multiplayer game

--- client/src/Pages/MultiplayerGame.jsx
+++ client/src/Pages/MultiplayerGame.jsx
@@ -15,10 +15,10 @@ const MultiplayerGame = () => {
     const socketContext = useContext(SocketContext);
     const location = useLocation();
     const [ended, setEnded] = useState(false);
-    
+
 
     useEffect(() => {
-        if(!locationContext.validAccess) {
+        if (!locationContext.validAccess) {
             navigate("/")
         }
     }, [locationContext.validAccess]);
@@ -26,7 +26,7 @@ const MultiplayerGame = () => {
     useEffect(() => {
         if (socketContext.socket.current) {
             updateListeners();
-        } 
+        }
     }, []);
 
     /**
@@ -55,6 +55,15 @@ const MultiplayerGame = () => {
         });
     }
 
+    /**
+     * Checks if the displayed user is the current user
+     * @param {Object} user 
+     * @returns {Boolean}
+     */
+    function checkUser(user) {
+        return user.id === socketContext.socket.current.id;
+    }
+
     return (
         <>
             < NavBar />
@@ -67,10 +76,11 @@ const MultiplayerGame = () => {
                         {socketContext.userList.map((user, i) => {
                             return <PlayerItem
                                 key={i} name={user.username}
-                                avatar={user.avatar} leader={i === 0} />
+                                avatar={user.avatar} leader={i === 0}
+                                myUser={checkUser(user)} />
                         })}
                     </div>
-                    <GameProgress/>
+                    <GameProgress />
                     <TypingScreen multiplayer={true} quote={location.state?.quote || ""} />
                 </div>
             }
